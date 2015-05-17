1e7d6284576b2efd1fa609ddf2875ec062aa4171
Pat Kira <pat@pat-kira.com>
2015-05-17 22:45:55 +1200

    Add images

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+$(function(){
+
+	$('.modal').on('shown.bs.modal', function () {
+		var flexslider = $(this).find('.flexslider');
+		$(flexslider).flexslider({
+			animation: "slide",
+			controlNav: "thumbnails"
+		});
+	});
+
+	$('#container').masonry({
+	    itemSelector : '.item'
+	});
+});
